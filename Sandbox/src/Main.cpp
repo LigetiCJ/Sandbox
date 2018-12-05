5a19dbfe0247651ccb601b6106d3ad787ccb98b5
noteclip <conner.ligeti@gmail.com>
2018-12-05 13:54:23 -0500

    messing around
    askjfsal

---
+++ Sandbox/src/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Main.h"
+#include "SDL2/SDL2.h"
+
+
+Main::Main()
+{
+    //ctor
+}
+
+Main::~Main()
+{
+    //dtor
+}
+
+
+int main(){
+    std::cout<<"Hello, World"<<std::endl;
+    return 0;
+}
